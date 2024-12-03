8b63e3d52b7d1060de53e042c568388d7eaa2b87
Haneef Muhammad <haneefmhmmd@gmail.com>
2024-12-02 23:56:28 -0500

    feat(issue-252): added soft delete feature for donor table

---
+++ 301247589_301276375_bright_aid_API/Migrations/Donor/20241203044747_AddedSoftDeleteToDonorTbl.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _301247589_301276375_bright_aid_API.Migrations.Donor
+{
+    /// <inheritdoc />
+    public partial class AddedSoftDeleteToDonorTbl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Donors",
+                type: "bit",
+                nullable: false,
+                defaultValue: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Donors");
+        }
+    }
+}
